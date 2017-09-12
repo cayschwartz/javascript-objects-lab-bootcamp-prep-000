9f371f2ff12ddfe398f11496debe900d7cfba500
Cayla Schwartz <cayschwartz@gmail.com>
2017-09-12 16:35:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ var recipe = {flavor:'strawberry'}
 
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object, { [key]: value})
-
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-
-
+  object[key]=value
+  return object
 }
 
 function deleteFromObjectByKey(object, key){
