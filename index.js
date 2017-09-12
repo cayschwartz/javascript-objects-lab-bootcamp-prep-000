a58f677d93d81a0562acbed0ae17e645a64dbc73
Cayla Schwartz <cayschwartz@gmail.com>
2017-09-12 16:30:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 function updateObjectWithKeyAndValue(object, key, value){
 return Object.assign({}, {key: value})
-  
+
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 
-  
+
 }
 
 function deleteFromObjectByKey(object, key){
 
-  
+
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
 
-  
+
 }
